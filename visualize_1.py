413910efb813e721ca732a99f23e41406b9f9b66
KevinLee <40953426+Kevinlee7@users.noreply.github.com>
2024-10-26 00:14:44 -0700

    Update visualize_1.py

--- visualize_1.py
+++ visualize_1.py
@@ -36,7 +36,7 @@ def plot_embedding(X, y, d, training_mode, save_name):
 
     # fig_name = 'saved_plot/' + str(training_mode) + '_' + str(save_name) + '.png'
     
-    fig_name = 'saved_plot/source_omg_source.png'
+    fig_name = 'saved_plot/DANN_omg_DANN.png'
     plt.savefig(fig_name)
     print('{} is saved'.format(fig_name))
 
