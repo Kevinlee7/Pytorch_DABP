21394cd15aebbcedbd09ee2ffbf4a05651b73160
KevinLee <40953426+Kevinlee7@users.noreply.github.com>
2024-10-25 21:51:36 -0700

    Update visualize_1.py

--- visualize_1.py
+++ visualize_1.py
@@ -13,7 +13,7 @@ import os
 source_test_loader = mnist.mnist_test_loader
 target_test_loader = mnistm.mnistm_test_loader
 
-def plot_embedding(X, y, d, training_mode):
+def plot_embedding(X, y, d, training_mode, save_name):
     x_min, x_max = np.min(X, 0), np.max(X, 0)
     X = (X - x_min) / (x_max - x_min)
     y = list(itertools.chain.from_iterable(y))
@@ -23,18 +23,24 @@ def plot_embedding(X, y, d, training_mode):
     for i in range(len(d)):  # X.shape[0] : 1024
         # plot colored number
         if d[i] == 0:
-            colors = (0.0, 0.0, 1.0, 1.0)  # 蓝色
+            colors = (0.0, 0.0, 1.0, 1.0)
         else:
-            colors = (1.0, 0.0, 0.0, 1.0)  # 红色
+            colors = (1.0, 0.0, 0.0, 1.0)
         plt.text(X[i, 0], X[i, 1], str(y[i]),
                  color=colors,
                  fontdict={'weight': 'bold', 'size': 9})
 
     plt.xticks([]), plt.yticks([])
-    plt.title(f'{training_mode} Feature Embedding')
+    if save_name is not None:
+        plt.title(save_name)
 
-    # 显示图像而不是保存
-    plt.show()
+    save_folder = 'saved_plot'
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
+    fig_name = 'saved_plot/' + str(training_mode) + '_' + str(save_name) + '.png'
+    plt.savefig(fig_name)
+    print('{} is saved'.format(fig_name))
 
 def visualize_input():
     source_test_loader = mnist.mnist_test_loader
@@ -89,6 +95,5 @@ def visualize_input():
     dann_tsne = tsne.fit_transform(combined_feature.detach().cpu().numpy())
     print('Draw plot ...')
     save_name = 'input_tsne_plot'
-    plot_embedding(dann_tsne, combined_label_list, combined_domain_list, 'input')
+    plot_embedding(dann_tsne, combined_label_list, combined_domain_list, 'input', 'mnist_n_mnistM')
 
-visualize_input()
